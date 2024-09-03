24fb6e5bf5f8efd219aa20b20934a5d7f2388d71
Nesma Osama <128908402+Nesma-Osama@users.noreply.github.com>
2024-09-03 23:15:50 +0300

    Update publicMiddelware.js
    change the name from error to message in json

--- CyberSecurity/middelware/publicMiddelware.js
+++ CyberSecurity/middelware/publicMiddelware.js
@@ -2,6 +2,6 @@ const net = require("net")
 function ipValidation(req, res, next) {
   const ip = req.body.ip;
   if (net.isIP(ip) !== 0) return next();
-  return res.status(400).json({ error: 'Invalid IP provided' });
+  return res.status(400).json({ message: 'Invalid IP provided' });
 }
 module.exports = { ipValidation }
