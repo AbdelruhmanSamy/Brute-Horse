58cf7bdb9736feaf1784f69ee4d805c51c4fca68
Nesma Osama <128908402+Nesma-Osama@users.noreply.github.com>
2024-09-20 19:49:40 +0300

    remove function

--- CyberSecurity/middelware/publicMiddelware.js
+++ CyberSecurity/middelware/publicMiddelware.js
@@ -1,5 +1,4 @@
 const net = require("net")
-const { v4: uuidv4 } = require('uuid');
 // this check the ip is correct
 function ipValidation(req, res, next) {
   const ip = req.body.ip;
@@ -7,9 +6,5 @@ function ipValidation(req, res, next) {
   return res.status(400).json({ message: 'Invalid IP provided' });
 }
 // this must call before execute the series of controllers
-function startProcess(req,res,next)//each user has a uniuq Id to craete a folder
-{
-   req.body.uniqueId = uuidv4();
-   return next()
-}
-module.exports = { ipValidation,startProcess }
+
+module.exports = { ipValidation }
