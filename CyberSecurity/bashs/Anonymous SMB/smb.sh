d4f5b89fd105d3fb0bb43020df07df2270e5dc5d
Nesma Osama <128908402+Nesma-Osama@users.noreply.github.com>
2024-09-21 13:19:56 +0300

    Update smb.sh

--- CyberSecurity/bashs/Anonymous SMB/smb.sh
+++ CyberSecurity/bashs/Anonymous SMB/smb.sh
@@ -13,10 +13,11 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 fi
 
 # Variables
-OUTPUT_DIR="scan_results_smb"
-TEXT_OUTPUT="$OUTPUT_DIR/smb.txt"
+
 IP_ADDRESS=$1        # The IP address is the first argument
 CHOICE=$2            # The action choice (1, 2, or 3) is the second argument
+OUTPUT_DIR=$3
+TEXT_OUTPUT="$OUTPUT_DIR/smb.txt"
 WORDLIST="/usr/share/wordlists/rockyou.txt"  # Path to the password wordlist file
 USERLIST="/usr/share/seclists/Usernames/top-usernames.txt"  # Common usernames from SecLists
 
@@ -34,32 +35,34 @@ case $CHOICE in
 
     # Check if the login was successful by searching the output
     if grep -q "Anonymous login successful" $TEXT_OUTPUT || grep -q "NT_STATUS_OK" $TEXT_OUTPUT; then
-      echo "Anonymous SMB login to $IP_ADDRESS succeeded!"
+     echo "The anonymous SMB login to $IP_ADDRESS has been successfully established. You can now access the server anonymously and navigate its resources freely." > $TEXT_OUTPUT
     else
-      echo "Anonymous SMB login to $IP_ADDRESS failed."
+      echo "Anonymous SMB login to $IP_ADDRESS failed." > $TEXT_OUTPUT
+      echo "The operation could not be completed successfully. Please check the following:
+    - Ensure the target IP address is indeed an SMB server.
+    - Confirm that the SMB server allows anonymous login if that was the chosen method." >> $TEXT_OUTPUT
     fi
     ;;
 
   2)
     # Option 2: Password breaking for a specific username
-    if [ -z "$3" ]; then
+    if [ -z "$4" ]; then
       echo "Error: You must provide a username for password breaking in choice 2."
       echo "Usage: $0 <IP_ADDRESS> 2 <USERNAME>"
       exit 1
     fi
-    username=$3
+    username=$4
     echo "Starting password breaking for username $username on SMB..."
 
     # Using Hydra for brute-forcing passwords with a known username on SMB
-    hydra -l $username -P $WORDLIST smb://$IP_ADDRESS -vV
+    hydra -l $username -P $WORDLIST SMB://$IP_ADDRESS -vV | grep -i "host" >  $TEXT_OUTPUT
     ;;
 
   3)
     # Option 3: Username and password brute-forcing on SMB
     echo "Starting username and password brute-forcing on SMB..."
-
+    hydra -L $USERLIST -P $WORDLIST smb://$IP_ADDRESS -vV | grep -i "host" > $TEXT_OUTPUT
     # Using Hydra to brute-force both usernames and passwords on SMB
-    hydra -L $USERLIST -P $WORDLIST smb://$IP_ADDRESS -vV
     ;;
 
   *)
@@ -70,4 +73,9 @@ case $CHOICE in
 esac
 
 # Clean up by removing the scan result text file
-rm $TEXT_OUTPUT
+# Check if the result file is empty and remove it if no content is found
+if [ ! -s $TEXT_OUTPUT ]; then
+echo "The operation did not complete successfully. The result file is empty, indicating that no valid login is found " >  $TEXT_OUTPUT
+else
+  echo "Results found and saved in $TEXT_OUTPUT."
+fi
