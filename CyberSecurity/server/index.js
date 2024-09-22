d65e670d8d56e998154f969005959b7bcc26a197
AbdelruhmanSamy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-09-22 15:04:18 +0300

    fix: adding cors to accept outside requests

--- CyberSecurity/server/index.js
+++ CyberSecurity/server/index.js
@@ -1,6 +1,9 @@
 const express=require("express")
 const app=express()
+const cors = require('cors');
 const vulnerabilityRouters=require("./router/vulnerabilityRouter")
+
+app.use(cors());
 app.use(express.json())
 app.use("/vulnerability",vulnerabilityRouters)
 app.listen(3000, () => {
