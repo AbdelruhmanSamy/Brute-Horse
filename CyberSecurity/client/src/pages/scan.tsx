5139622b7bac4d12e26939c4265e92de2aa7b986
AbdelruhmanSamy <abdelruhman.mohamad04@eng-st.cu.edu.eg>
2024-09-22 07:46:49 +0300

    scan page finished

---
+++ CyberSecurity/client/src/pages/scan.tsx
@@ -0,0 +1,21 @@
+import { FormControl, RadioGroup , FormControlLabel, Radio, FormLabel} from '@mui/material'
+import React from 'react'
+import FormContainer from '../components/FormContainer'
+import { formData } from '../data/data'
+
+function Scan() {
+
+    return (    
+        <div 
+        className="
+        w-full
+        bg-gradient-to-r from-regular-blue from-60% via-light-blue via-80% to-regular-blue to-95%
+        flex
+        justify-center
+        ">
+            <FormContainer {...formData}/>
+        </div>
+    )
+}
+
+export default Scan
